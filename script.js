c239d11528646f8abc60bb9c3848b2359277f304
robhaj <hajek.rob@gmail.com>
2015-07-10 16:00:00 -0600

    Added index.html and script.js to complete javascript-loops exercise

---
+++ script.js
@@ -0,0 +1,20 @@
+for (i=0; i<= 1000; i+=100) {
+console.log(i);
+}
+for (i=1; i<=128; i*=2) {
+  console.log(i)
+}
+for (i=0; i<=10; i+=2) {
+  console.log(i)
+}
+for (i=3; i<=15; i+=3) {
+  console.log(i)
+}
+for (i=9; i>=0; i-=1) {
+  console.log(i)
+}
+for ( j = 0; j<=3; j++ ) {
+for ( k = 0; k<=4; k++ ) {
+  console.log(k);
+}
+}
