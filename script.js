13457445beafc7efa8a0eddf3421b3212b3c9403
robhaj <hajek.rob@gmail.com>
2015-07-11 09:50:27 -0600

    refactor #6

--- script.js
+++ script.js
@@ -2,16 +2,26 @@ for (i=0; i<= 1000; i+=100) {
 console.log(i);
 }
 for (i=1; i<=128; i*=2) {
-  console.log(i)
+  console.log(i);
 }
 for (i=0; i<=10; i+=2) {
-  console.log(i)
+  console.log(i);
 }
 for (i=3; i<=15; i+=3) {
-  console.log(i)
+  console.log(i);
 }
 for (i=9; i>=0; i-=1) {
-  console.log(i)
+  console.log(i);
+}
+var i = 1
+for (k = 0; k < 3; k++) {
+  console.log(i);
+}
+for (j = 0 ; j < 3; j++) {
+var i = i + 1;
+for (k = 0; k< 3; k++) {
+console.log(i);
+}
 }
 for ( j = 0; j<=3; j++ ) {
 for ( k = 0; k<=4; k++ ) {
